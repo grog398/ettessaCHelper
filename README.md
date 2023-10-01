59dfc08277f0c8eb0abdfd590e86f547a75f6235
grog <92819494+grog398@users.noreply.github.com>
2023-10-01 11:54:09 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,20 @@
+﻿# ettessaCHelper: Better Cassette Music
+## Version 1.0
+### About
+ettessaCHelper provides a better experience when trying to import your music of choice into Celeste to be used as a cassette song. The default cassette music system is not optimized for full songs and can cause music to sound very glitchy when importing a song, however ettessaCHelper rewrites this system to sound better when importing whole songs to be used as cassette music.
+
+### Disclaimer
+This is my first code mod. I made it specifically for one of my solo projects. It still has a tendency to desync after a while or with excessive pausing.
+
+### Use
+
+1. Get a .wav file of your song
+2. Follow [the guide for cassette music](https://github.com/EverestAPI/Resources/wiki/Advanced-Custom-Audio#adding-cassette-music) on the Everest Wiki, skipping steps 3-7
+3. Create a new parameter, named "autosync", with a max value 1 more than the value of "Max Beats" in Loenn.
+4. Copy-paste your .wav file into the track of the "autosync" parameter, and scale it to fill the entire track.
+5. Select the sample, and expand the external sample editor so that the "starting position" knob is visible.
+6. Right click on the knob and select "Add Automation", and add 2 points at each end from 0 to whatever your maximum value is.
+7. Now changing the value of "autosync" should change where playback starts when you hit play.
+8. Build your bank just like you would normally, and then set the cassette music in Loenn to your cassette song, and you should be good to go!
+
+
---
+++ ettessaCHelperModule.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Celeste.Mod.Entities;
+using Monocle;
+using System.Collections.Generic;
+using FMOD.Studio;
+using MonoMod.Utils;
+
+namespace Celeste.Mod.ettessaCHelper {
+    public class ettessaCHelperModule : EverestModule {
+        public static ettessaCHelperModule Instance { get; private set; }
+
+        private Level level;
+        private int numSyncs;
+
+        private bool isPaused;
+
+        private float sixnote;
+        private EventInstance sfx;
+
+        bool dontRepeatSixteenTimesPlease = true;
+
+        CassetteBlockManager current;
+        DynamicData cassetteManagerData;
+        private String music;
+
+        public override Type SettingsType => typeof(ettessaCHelperModuleSettings);
+        public static ettessaCHelperModuleSettings Settings => (ettessaCHelperModuleSettings) Instance._Settings;
+
+        public override Type SessionType => typeof(ettessaCHelperModuleSession);
+        public static ettessaCHelperModuleSession Session => (ettessaCHelperModuleSession) Instance._Session;
+
+        public ettessaCHelperModule() {
+            Instance = this;
+#if DEBUG
+            // debug builds use verbose logging
+            Logger.SetLogLevel(nameof(ettessaCHelperModule), LogLevel.Verbose);
+#else
+            // release builds use info logging to reduce spam in log files
+            Logger.SetLogLevel(nameof(ettessaCHelperModule), LogLevel.Info);
+#endif
+        }
+
+        public override void Load() {
+            
+            Logger.SetLogLevel("ettessaCHelper", LogLevel.Debug);
+            On.Celeste.CassetteBlockManager.AdvanceMusic += BetterAdvance;
+            Everest.Events.Level.OnTransitionTo +=  GetMusic;
+            Everest.Events.Level.OnPause += StopMusicOnPause;
+        }
+
+        public override void Unload() {
+            On.Celeste.CassetteBlockManager.AdvanceMusic -= BetterAdvance;
+            Everest.Events.Level.OnTransitionTo -=  GetMusic;
+            Everest.Events.Level.OnPause -= StopMusicOnPause;
+        }
+
+
+        public void BetterAdvance(On.Celeste.CassetteBlockManager.orig_AdvanceMusic orig, CassetteBlockManager s, float t) {
+            if (numSyncs % 2 == 0) { orig(s, t); }
+            if (sfx == null) {
+                current = Celeste.Scene.Tracker.GetEntity<CassetteBlockManager>();
+                cassetteManagerData = new DynamicData(current);
+                sfx = cassetteManagerData.Get<EventInstance>("sfx");
+            }
+            if (current.GetSixteenthNote() % 256 == 0 && dontRepeatSixteenTimesPlease || isPaused && dontRepeatSixteenTimesPlease) {
+                // sfxTemp = sfx;
+                //float currSixteenth;
+                music = Audio.GetEventName(sfx);
+                Audio.BusStopAll("bus:/music/tunes/cassette");
+                sfx.setProperty(EVENT_PROPERTY.MAXIMUM_DISTANCE, cassetteManagerData.Get<int>("beatIndexMax"));
+                //sfx.getParameterValue("autosync", out currSixteenth, out temp);
+                //Logger.Log(LogLevel.Debug, "ettessaCHelper", "sixteenthNote on sfx before set: " + currSixteenth);
+                //Logger.Log(LogLevel.Debug, "ettessaCHelper", "sixteenthNote on CassetteBlockManager before: " + current.GetSixteenthNote());
+                sixnote = current.GetSixteenthNote();
+                sfx.setParameterValue("autosync", sixnote);
+                sfx.start();
+                //Logger.Log(LogLevel.Debug, "ettessaCHelper", "maxBeats: " + cassetteManagerData.Get<int>("beatIndexMax"));
+                numSyncs++;
+                //sfx.getParameterValue("autosync", out currSixteenth, out temp);
+                //Logger.Log(LogLevel.Debug, "ettessaCHelper", "sixteenthNote on sfx: " + currSixteenth);
+                //Logger.Log(LogLevel.Debug, "ettessaCHelper", "sixteenthNote on CassetteBlockManager: " + current.GetSixteenthNote());
+                dontRepeatSixteenTimesPlease = false;
+                isPaused = false;
+            } else if (current.GetSixteenthNote() % 64 != 0) {
+                dontRepeatSixteenTimesPlease = true;
+            }
+            if (numSyncs % 2 == 1) { orig(s, t); }
+        }
+        public void GetMusic(Level level, LevelData next, Vector2 direction) {
+            music = next.AltMusic;
+            this.level = level;
+        }
+
+        public void StopMusicOnPause(Level level, int startIndex, bool minimal, bool quickReset) {
+            if (sfx != null) {
+                Audio.BusStopAll("bus:/music/tunes/cassette");
+            }
+            isPaused = true;
+        }
+    }
+}
